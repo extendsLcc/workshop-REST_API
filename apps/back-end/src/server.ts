df9193733fb28ac27537b6abb090a634f616a703
extendsLcc <lucas.lcc@hotmail.com>
2022-09-01 19:56:32 -0300

    chore(prisma): setup prisma client as fastify plugin

--- apps/back-end/src/server.ts
+++ apps/back-end/src/server.ts
@@ -1,14 +1,13 @@
 import { loggerConfig } from './logger';
 import { fastify as fastifyFactory } from 'fastify';
 import { categoryRoutes } from './routes/categories.routes';
-import { productsRoutes } from './routes/products.routes';
-import { customerRoutes } from './routes/customers.routes';
-import { ordersRoutes } from './routes/orders.routes';
+import fastifyPrismaClient from 'fastify-prisma-client';
 
 const fastify = fastifyFactory({
   logger: loggerConfig,
 });
 
+fastify.register(fastifyPrismaClient, {});
 fastify.register(categoryRoutes);
 fastify.register(productsRoutes);
 fastify.register(customerRoutes);
