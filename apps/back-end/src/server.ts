0247f756c19e4cfdab2d813ac72f40f992277495
extendsLcc <lucas.lcc@hotmail.com>
2022-09-01 20:57:32 -0300

    refactor(customer): refactors customer endpoints to use prisma instead of in-memory array

--- apps/back-end/src/server.ts
+++ apps/back-end/src/server.ts
@@ -2,7 +2,7 @@ import { loggerConfig } from './logger';
 import { fastify as fastifyFactory } from 'fastify';
 import { categoryRoutes } from './routes/categories.routes';
 // import { productsRoutes } from './routes/products.routes';
-// import { customerRoutes } from './routes/customers.routes';
+import { customerRoutes } from './routes/customers.routes';
 // import { ordersRoutes } from './routes/orders.routes';
 import fastifyPrismaClient from 'fastify-prisma-client';
 
@@ -14,7 +14,7 @@ fastify.register(fastifyPrismaClient, {});
 
 fastify.register(categoryRoutes);
 // fastify.register(productsRoutes);
-// fastify.register(customerRoutes);
+fastify.register(customerRoutes);
 // fastify.register(ordersRoutes);
 
 export { fastify };
