3fcc01a345eb967e2ff678e6cd5b75c55270474f
extendsLcc <lucas.lcc@hotmail.com>
2022-09-01 21:12:32 -0300

    fix(prisma): add missing status column to product model

---
+++ apps/back-end/prisma/migrations/20220902001134_add_status_to_product_table/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - Added the required column `status` to the `Product` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Product" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "stock" INTEGER NOT NULL,
+    "status" BOOLEAN NOT NULL,
+    "categoryId" INTEGER NOT NULL,
+    CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Product" ("categoryId", "description", "id", "name", "price", "stock") SELECT "categoryId", "description", "id", "name", "price", "stock" FROM "Product";
+DROP TABLE "Product";
+ALTER TABLE "new_Product" RENAME TO "Product";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
