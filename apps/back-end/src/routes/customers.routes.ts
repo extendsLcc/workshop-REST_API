1e3e2e5068420b6f2034b861b388c85fb7bfebf9
extendsLcc <lucas.lcc@hotmail.com>
2022-09-04 16:48:28 -0300

    refactor(customer): desestruturate request body to better data safeness

--- apps/back-end/src/routes/customers.routes.ts
+++ apps/back-end/src/routes/customers.routes.ts
@@ -6,15 +6,20 @@ import HttpStatus from 'http-status';
 async function customerRoutes(fastify: FastifyInstance) {
   const customerService = new CustomerService(fastify.prisma);
   // List all customers
-  fastify.get('/customers', () => {
-    return customerService.listCustomers();
+  fastify.get('/customers', async () => {
+    return await customerService.listCustomers();
   });
 
   // Create a new customer
   fastify.post<{
     Body: Prisma.CustomerCreateInput;
   }>('/customers', async (request, reply) => {
-    return reply.status(HttpStatus.CREATED).send(await customerService.createCustomer(request.body));
+    const { name } = request.body;
+    return reply.status(HttpStatus.CREATED).send(
+      await customerService.createCustomer({
+        name,
+      }),
+    );
   });
 }
 
