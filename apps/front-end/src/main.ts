154e69eb0d98a561714ceb32734ddb38a3d2ded3
extendsLcc <lucas.lcc@hotmail.com>
2022-09-06 18:32:23 -0300

    chore(vue-setup): config pages and vue-layouts vite-plugins

--- apps/front-end/src/main.ts
+++ apps/front-end/src/main.ts
@@ -1,4 +1,11 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
+import routes from '~pages';
 
-createApp(App).mount('#app');
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount('#app');
--- apps/front-end/vite.config.ts
+++ apps/front-end/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import Pages from 'vite-plugin-pages';
+import Layouts from 'vite-plugin-vue-layouts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      reactivityTransform: true,
+    }),
+    Pages({ extensions: ['vue'] }),
+    Layouts(),
+  ],
 });
