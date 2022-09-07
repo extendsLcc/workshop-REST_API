bd6289436fa5dd4c36c7ccf64ff6d4b2a2695677
extendsLcc <lucas.lcc@hotmail.com>
2022-09-06 21:21:07 -0300

    chore(layouts): setup vue layout plugin

--- apps/front-end/src/main.ts
+++ apps/front-end/src/main.ts
@@ -1,11 +1,14 @@
 import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
+import { setupLayouts } from 'virtual:generated-layouts';
 import App from './App.vue';
-import routes from '~pages';
+import generatedRoutes from '~pages';
 
 import '@unocss/reset/tailwind.css';
 import '@kidonng/daisyui/index.css';
 import 'uno.css';
+
+const routes = setupLayouts(generatedRoutes);
 
 const router = createRouter({
   history: createWebHistory(),
