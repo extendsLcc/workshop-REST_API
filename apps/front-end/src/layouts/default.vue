77b2369dfac085db2bb0cf4f861e30d0e14897f5
extendsLcc <lucas.lcc@hotmail.com>
2022-09-08 03:13:05 -0300

    feat(layout): created base front-end app layout

--- apps/front-end/src/layouts/default.vue
+++ apps/front-end/src/layouts/default.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+  import SideNav from '@/components/SideNav.vue';
+  import NavBar from '@/components/NavBar.vue';
+
+  const isDrawerOpen = $ref(false);
+</script>
+
 <template>
-  <RouterView />
+  <section class="drawer drawer-mobile">
+    <input id="aside-drawer" v-model="isDrawerOpen" type="checkbox" class="drawer-toggle" />
+    <SideNav class="drawer-side">
+      <label for="aside-drawer" class="drawer-overlay z-1"></label>
+    </SideNav>
+    <section class="drawer-content flex flex-col">
+      <NavBar :is-drawer-open="isDrawerOpen" />
+      <RouterView />
+    </section>
+  </section>
 </template>
---
+++ apps/front-end/src/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="hero h-full bg-base-200">
+    <main class="hero-content text-center border-none backdrop-blur-2xl bg-transparent">
+      <article class="max-w-md">
+        <h1 class="text-5xl font-bold">Order API</h1>
+        <p class="py-6">
+          Front-end utilizando Vue.js e daisyUI consumindo API RESTful de pedidos desenvolvida para o workshop de API
+          rest, demonstrando o consumo de API via através de uma SPA.
+        </p>
+        <a href="https://github.com/extendsLcc/Workshop-REST_API_for_beginners_with_nodejs" target="_blank">
+          <button class="btn btn-primary" type="button">
+            <i class="i-mdi-github mr-2"></i>
+            Source
+          </button>
+        </a>
+      </article>
+    </main>
+  </section>
+</template>
