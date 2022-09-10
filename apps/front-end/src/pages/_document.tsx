76997f34854783993772d888dc7a13aef946ad92
ndelanhese <nnathanh3@gmail.com>
2022-09-10 15:09:50 -0300

    fix(_document): fix the link tag (google font)

--- apps/front-end/src/pages/_document.tsx
+++ apps/front-end/src/pages/_document.tsx
@@ -8,9 +8,9 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <Link rel="preconnect" href="https://fonts.googleapis.com" />
-          <Link rel="preconnect" href="https://fonts.gstatic.com" />
-          <Link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" />
         </Head>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <body>
