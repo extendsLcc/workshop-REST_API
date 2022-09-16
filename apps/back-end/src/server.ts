27fd41122f942c0ca04b47944c89a12cc4a2780f
extendsLcc <lucas.lcc@hotmail.com>
2022-09-15 23:32:26 -0300

    feat(crud): add crud example of users resource

--- apps/back-end/src/server.ts
+++ apps/back-end/src/server.ts
@@ -2,30 +2,84 @@ import { fastify as fastifyFactory } from 'fastify';
 
 export const fastify = fastifyFactory({});
 
-fastify.get('/workshop', async (request, reply) => {
+type User = {
+  id: number;
+  name: string;
+  age: number;
+};
+
+const users: User[] = [
+  {
+    id: 1,
+    name: 'John Doe',
+    age: 30,
+  },
+  {
+    id: 2,
+    name: 'Jane Doe',
+    age: 28,
+  },
+];
+
+fastify.get('/users', async (request, reply) => {
   return {
-    lecturer: 'extendslcc',
-    workshop: 'api-rest-basics',
-    property: 'value',
+    users,
   };
 });
 
-fastify.get('/query', async (request, reply) => {
-  return {
-    queryParams: request.query,
+fastify.get<{
+  Params: {
+    id: string;
   };
+}>('/users/:id', async (request, reply) => {
+  const { id } = request.params;
+  const user = users.find((user) => user.id === Number(id));
+  return user;
 });
 
-fastify.post('/post', async (request, reply) => {
-  return {
-    body: request.body,
+fastify.post<{
+  Body: {
+    name: string;
+    age: number;
   };
+}>('/users', async (request, reply) => {
+  const { name, age } = request.body;
+  const newUser = { id: Date.now(), name, age };
+  users.push(newUser);
+  return newUser;
 });
 
-fastify.get('/route/:id', async (request, reply) => {
-  return {
-    routeParams: request.params,
+fastify.put<{
+  Params: {
+    id: string;
   };
+  Body: {
+    name: string;
+    age: number;
+  };
+}>('/users/:id', async (request, reply) => {
+  const { id } = request.params;
+  const { name, age } = request.body;
+  const updatingUser = users.find((user) => user.id === Number(id));
+  if (!updatingUser) {
+    return reply.code(404).send({ message: 'User not found' });
+  }
+  Object.assign(updatingUser, { name, age });
+  return updatingUser;
+});
+
+fastify.delete<{
+  Params: {
+    id: string;
+  };
+}>('/users/:id', async (request, reply) => {
+  const { id } = request.params;
+  const deletingUser = users.find((user) => user.id === Number(id));
+  if (!deletingUser) {
+    return reply.code(404).send({ message: 'User not found' });
+  }
+  users.splice(users.indexOf(deletingUser), 1);
+  return { message: 'User deleted' };
 });
 
 const start = async () => {
